02915ab70a413bcf3264ea54471bf82112905d41
DiamondCrack <zk05161219@gmail.com>
2017-11-21 11:02:19 +0800

    Site updated: 2017-11-21 11:02:14

--- match-game/js/main.js
+++ match-game/js/main.js
@@ -1,3 +1,4 @@
+//------------------数据------------------------
 data = [
     {id:0,name:'github',matchId:1},
     {id:1,name:'github',matchId:1},
@@ -20,16 +21,29 @@ data = [
     {id:18,name:'youtube-play',matchId:10},
     {id:19,name:'youtube-play',matchId:10}
 ];
-
+//-------------------------全局变量----------------------------------
 global = {
     listCon: document.querySelector('.list-con'),
-    judgeArr: []
+    prompt: document.querySelector('.prompt'),
+    judgeArr: [],
+    time: {
+	h:0,
+	m:0,
+	s:0,
+	t:0,
+	timer:''
+    },
+    correct: false,
+    wrongStep: 0,
+    totleStep:0,
+    starsNum: 0
 };
 
+//-----------------------------生成节点--------------------------------------
 function createLiNode(dataItem){
     const item = document.createElement('li');
     item.matchId = dataItem.matchId;
-    item.ElementId = dataItem.id;
+    item.elementId = dataItem.id;
     item.correct = false;
     const str = `
 	    <div class="card flipper-container">
@@ -46,8 +60,9 @@ function createLiNode(dataItem){
 
     return item;
 }
-
+//---------------------------生成结构------------------------------------------------------
 function createCards(data){
+    global.listCon.innerHTML = '';
     const arr = data.slice().sort(function(){
 	return Math.random() - 0.5;
     });
@@ -56,18 +71,20 @@ function createCards(data){
     });
 }
 
-createCards(data);
+// createCards(data);
 
+//--------------------------点击翻转函数--------------------------------------------------------------
 function rotate(){
     const listCon = document.querySelector('.list-con');
     const items = listCon.querySelectorAll('li');
+
     [...items].forEach(function(item){
 	decide(item);
     });
 };
 
-rotate();
-
+// rotate();
+//---------------------------判断是否匹配正确----------------------------------------------------
 
 function decide(item){
     const flipper = item.querySelector('.flipper');
@@ -76,23 +93,28 @@ function decide(item){
 	if(!item.correct){
 	    global.judgeArr.push(item);
 	    if(global.judgeArr.length== 2){
-		if(global.judgeArr[0].matchId !== global.judgeArr[1].matchId){
-		    judge(global.judgeArr,false);	    
+		if(global.judgeArr[0].elementId===global.judgeArr[1].elementId) {
+		    global.judgeArr.length = 1;
+		}else if(global.judgeArr[0].matchId !== global.judgeArr[1].matchId){
+		    judge(global.judgeArr,false);
+		    global.judgeArr.length=0;
+		    global.wrongStep++;
 		}else{
-		    console.log('对了');
-		    item.correct = true;
+		    global.judgeArr.forEach(item=>item.correct = true);
 		    judge(global.judgeArr,true);
+		    global.judgeArr.length=0;
 		}
-		global.judgeArr.length=0;
+		global.totleStep++;
+		stars();
+		prompt();
 	    }
 	}
     };
 }
 
-
+//------------------------提示是否匹配正确-------------------------------
 function judge(arr,onOff){
     global.judgeArr.forEach(function(item){
-	console.log(item);
 	const flipper = item.querySelector('.flipper');
 	if(!onOff){
 	    setTimeout(function(){
@@ -106,3 +128,103 @@ function judge(arr,onOff){
 	}
     });
 }
+//--------------------------计时器------------------------------------------
+function timing(){
+    global.time.h = parseInt(global.time.t/60/60);
+    global.time.m = parseInt(global.time.t/60%60);
+    global.time.s = parseInt(global.time.t%60);
+
+    const hours = document.querySelector('.hours');
+    const minutes = document.querySelector('.minutes');
+    const seconds = document.querySelector('.seconds');
+
+    hours.innerHTML = global.time.h ? global.time.h : '';
+    minutes.innerHTML = add0(global.time.m);
+    seconds.innerHTML = add0(global.time.s);
+
+    global.time.t++;
+
+    global.time.timer = setTimeout(timing,1000);
+}
+function add0(num){
+    return num < 10 ?  '0'+num : ''+num;
+}
+// timing();
+
+//--------------------------星星-------------------------------
+function stars(){
+    const  star = document.querySelectorAll('.fa-star');
+    const starsArr = [...star];
+    let arr = starsArr.slice();
+    if(global.wrongStep <= 5){
+	arr.length =0;
+	global.starsNum = 3;
+    }else if(global.wrongStep > 5 && global.wrongStep<= 10){
+	arr = arr.slice(2,3);
+	global.starsNum = 2;
+    } else if(global.wrongStep <= 15&&global.wrongStep>10){
+	arr.shift();
+	global.starsNum = 1;
+    }else{
+	global.starsNum =0;
+    }
+    arr.forEach(item=>item.className='fa fa-star-o');
+}
+//----------------------------提示框---------------------------------
+function prompt(){
+    // global.prompt.classList.toggle('show',correct);
+    const listCon = document.querySelector('.list-con');
+    const items = listCon.querySelectorAll('li');
+    const getStar = global.prompt.querySelector('.get-star em');
+    const total = global.prompt.querySelector('.total-step em');
+    global.correct = [...items].every(function(item){
+	return item.correct;
+    });
+    console.log(global.correct);
+    total.innerHTML = global.totleStep;
+    getStar.innerHTML = global.starsNum;
+    global.prompt.classList.toggle('show',global.correct);
+    if(global.correct){
+	clearTimeout(global.time.timer);
+	again(global.correct,data);
+    }
+
+}
+//----------------------------初始化--------------------------------
+function gameInit(data){
+    createCards(data);
+    rotate();
+    timing();
+    global.wrongStep = 0;
+    global.totleStep = 0;
+    global.starsNum = 0;
+    global.time ={
+	h:0,
+	m:0,
+	s:0,
+	t:0,
+	timer:''
+    };
+    global.correct = false;
+    global.prompt.classList.remove('show');
+    const  star = document.querySelector('.star');
+    const starsAll = star.querySelectorAll('.fa');
+    [...starsAll].forEach(function(item){
+	console.log(item);
+	    item.className = 'fa fa-star';
+    });
+}
+
+//-------------------------------再来一次---------------------------------
+function again(correct,data){
+    const btnAgain = document.querySelector('.once-more');
+
+    btnAgain.onclick = function(){
+	gameInit(data);
+	
+    };
+
+}
+
+
+gameInit(data);
